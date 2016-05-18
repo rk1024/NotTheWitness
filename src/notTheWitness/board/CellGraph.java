cfe869d3a1ae47f785078e1fffd8eef135a0b4f7
rk1024 <rookie1286@gmail.com>
2016-05-18 09:50:04 -0700

    Continued work on cell graph.

--- src/notTheWitness/board/CellGraph.java
+++ src/notTheWitness/board/CellGraph.java
@@ -8,10 +8,41 @@ public class CellGraph {
   
   public CellGraph(NodeGraph borderGraph) {
     this.borderGraph = borderGraph;
+    
+    HashSet<Node> pool = new HashSet<Node>();
+    for (Node node : borderGraph.getNodes()) pool.add(node);
+    
+    ArrayDeque<Node> queue = new ArrayDeque<Node>();
+    queue.addLast(borderGraph.getNodes().iterator().next());
+    
+    Node node;
+    while ((node = queue.pollFirst()) != null) {
+      for (Edge edge : borderGraph.getEdges(node)) {
+        Node other = edge.other(node);
+        
+        if (pool.contains(other))
+          queue.addLast(other);
+      }
+    }
   }
   
   public Iterable<Cell> getCells() { return cells; }
   public Iterable<Node> getNodes() { return borderGraph.getNodes(); }
   public Iterable<Edge> getEdges() { return borderGraph.getEdges(); }
   public Iterable<Edge> getEdges(Node connectedTo) { return borderGraph.getEdges(connectedTo); }
+  
+  public HashSet<Cell> getCells(Node[] bordering) {
+    HashSet<Cell> set = new HashSet<Cell>();
+    
+    cellLoop: for (Cell cell : cells) {
+      for (Node node : bordering) {
+        if (!cell.getNodes().contains(node))
+          continue cellLoop;
+      }
+      
+      set.add(cell);
+    }
+    
+    return set;
+  }
 }
