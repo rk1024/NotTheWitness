18ae2077d732da4cc3961981dd5a58127078bb62
rk1024 <rookie1286@gmail.com>
2016-05-18 09:50:04 -0700

    Continued work on cell graph.

--- src/notTheWitness/board/Cell.java
+++ src/notTheWitness/board/Cell.java
@@ -1,8 +1,16 @@
 package notTheWitness.board;
 
+import java.util.*;
+
 public class Cell extends Node {
+  private HashSet<Node> nodes = new HashSet<Node>();
+  private HashSet<Edge> edges = new HashSet<Edge>();
+  
   public Cell(int x, int y) {
     super(x, y);
     // TODO Auto-generated constructor stub
   }
+  
+  public HashSet<Node> getNodes() { return nodes; }
+  public HashSet<Edge> getEdges() { return edges; }
 }
